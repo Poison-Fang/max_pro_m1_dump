e2fcab077761723051d7f2a301afb97c8db47aab
Poison-Fang <ayushwalia999@gmail.com>
2019-07-31 06:19:44 +0000

    WW-16.2017.1906.056

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:55231788:60e5e07fd08ffff81245a35ec599750e0ce92afc; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:51180840:3fb9976c8cdc212310923dfcbdf927a0a3dd0384 EMMC:/dev/block/bootdevice/by-name/recovery 60e5e07fd08ffff81245a35ec599750e0ce92afc 55231788 3fb9976c8cdc212310923dfcbdf927a0a3dd0384:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:55231788:53b75ef40482f1c96b1dedb4786c345c18c24a11; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:51180840:fc5de4ba90e29d0c2c04e26f3c96c0c147aca68a EMMC:/dev/block/bootdevice/by-name/recovery 53b75ef40482f1c96b1dedb4786c345c18c24a11 55231788 fc5de4ba90e29d0c2c04e26f3c96c0c147aca68a:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
